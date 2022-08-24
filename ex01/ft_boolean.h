884252b1a7ddedf19531301185580af3ab37e494
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-24 15:08:03 +0200

    finish ex02 ft_boolean

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 14:44:17 by aantonio          #+#    #+#             */
+/*   Updated: 2022/08/24 15:06:15 by aantonio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+typedef int	t_bool;
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define EVEN(x) (x % 2)
+
+#endif
